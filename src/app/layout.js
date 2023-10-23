aac2f976de3529f9709519d7b313a2b26af9b4ed
지우 <137967148+devjyu@users.noreply.github.com>
2023-10-23 15:04:20 +0900

    test start

---
+++ src/app/layout.js
@@ -0,0 +1,14 @@
+import "/app/globals.css";
+
+export const metadata = {
+  title: "Every Ai",
+  description: "ai 창작물 전문 플랫폼",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="kr">
+      <body>{children}</body>
+    </html>
+  );
+}

9c5ae7c2cb4466c084afefbdf7d7a8c6e346facf
지우 <137967148+devjyu@users.noreply.github.com>
2023-10-26 14:38:40 +0900

    메인 광고 수정중

--- src/app/main/_components/advertisement-banner.js
+++ src/app/main/_components/advertisement-banner.js
@@ -1,9 +1,25 @@
+import React, {useState, useEffect} from 'react';
+import Data from '@/repository/data/main.json';
+import Image from 'next/image';
+
 export default function MainAdvertisementBanner() {
+
+  const [advertisementBanner, setAdvertisementBanner] = useState([]);
+
+  useEffect(() => {
+    fetch('data/main.json')
+    .then((res) => res.json())
+    .then((result) => setAdvertisementBanner(result));
+  }, []);  
+
   return (
+    // <div className="main-banner-box">
+    //   <div className="main-inner-banner-box">
+    //     <Image src="" alt='광고배너'/>
+    //   </div>
+    // </div>
     <div className="main-banner-box">
-      <div className="main-inner-banner-box">
-        <div>광고배너</div>
-      </div>
+      <Image src="" alt='광고배너' width={700} height={100} />
     </div>
   );
 }

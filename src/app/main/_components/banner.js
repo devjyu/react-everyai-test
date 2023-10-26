a76e786c8c9ae16698e5bb257f741e072696ebce
지우 <137967148+devjyu@users.noreply.github.com>
2023-10-26 20:32:35 +0900

    banner 완성

--- src/app/main/_components/banner.js
+++ src/app/main/_components/banner.js
@@ -1,16 +1,23 @@
 import Image from "next/image";
 import { useEffect, useState, useRef } from 'react';
+import Link from "next/link";
 
-export default function AdvertisementBanner({ data }) {
+export default function AdvertisementBannerTest({ data }) {
     const [currentInedex, setCurrentIndex] = useState(0);
     const bannerSliderInnerRef = useRef(null);
     const bannerSliderWidth = useRef(0);
+    const bannerSliderClone = useRef(null); // Use useRef for sliderClone
 
     useEffect(() => {
-        const bannerSliderImg = document.querySelector('.banner-slider_img');
-        bannerSliderInnerRef.current = document.querySelector('.banner-slider_inner');
+        const bannerSliderWrap = document.querySelector('.banner-slider-wrap');
+        const bannerSliderImg = document.querySelector('.banner-slider_img'); //visible area
+        bannerSliderInnerRef.current = document.querySelector('.banner-slider_inner'); //moving area
+        const bannerSlider = document.querySelectorAll('.banner-slider'); //individual image
 
-        bannerSliderWidth.current = bannerSliderImg.offsetWidth;
+        bannerSliderWidth.current = bannerSliderImg.offsetWidth; //Get image width value
+        bannerSliderClone.current = bannerSliderInnerRef.current.firstElementChild.cloneNode(true); // Store the value in useRef
+        bannerSliderInnerRef.current.appendChild(bannerSliderClone.current); //Insert the first image last
+
         const sliderEffect = () => {
             setCurrentIndex((prevIndex) => {
                 let nextIndex = prevIndex + 1;
@@ -19,8 +26,8 @@ export default function AdvertisementBanner({ data }) {
 
                 if (nextIndex === data.advertisementAdminBannerList.length) {
                     setTimeout(() => {
-                        bannerSliderInnerRef.current.style.transition = '0s';
-                        bannerSliderInnerRef.current.style.transform = 'translateX(0px)';
+                        bannerSliderInnerRef.current.style.transition = '0s'; //No animation
+                        bannerSliderInnerRef.current.style.transform = 'translateX(0px)'; //Initialize to 0px
                     }, 700);
 
                     nextIndex = 0;
@@ -30,28 +37,33 @@ export default function AdvertisementBanner({ data }) {
         };
 
         const interval = setInterval(sliderEffect, 4000);
+
         return () => clearInterval(interval);
     }, [data.advertisementAdminBannerList.length]);
-    
+
     const handleImageClick = (index) => {
-        window.location.href = `main/banner/${index}`;
+        window.location.href = `main/baner/${index}`;
     };
 
     return (
-        <div className="banner-slider-wrap">
-            <div className="banner-slider_inner">
-                {data.advertisementAdminBannerList.map((thumbnail, index) => (
-                    <div className="banner-slider" key={index} onClick={() => handleImageClick(index)}>
-                        <Image
-                            src={thumbnail.thumbnailFileEntity.subFileList[0].url}
-                            unoptimized={true}
-                            alt="광고배너"
-                            width={700}
-                            height={100}
-                        />
+        <>
+            <div class="banner-slider-wrap">
+                <div className="banner-slider_img">
+                    <div className="banner-slider_inner">
+                        {data.advertisementAdminBannerList.map((thumbnail, index) => (
+                            <div className="banner-slider" key={index} onClick={() => handleImageClick(index)}>
+                                <Image
+                                src={thumbnail.thumbnailFileEntity.subFileList[0].url}
+                                unoptimized={true}
+                                alt="광고배너"
+                                width={700}
+                                height={100}
+                                />
+                            </div>
+                        ))}
                     </div>
-                ))}
+                </div>
             </div>
-        </div>
+        </>
     );
 }
